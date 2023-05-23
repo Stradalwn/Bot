3c616b7fb82dad84916a3f5c06ca0077366ba929
Stradalwn <132915496+Stradalwn@users.noreply.github.com>
2023-05-24 04:27:51 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
         'ADMINS', '6069226956 1288398723').split()]
     API_HASH = os.environ.get("API_HASH", "5078bf823d7e8248776f5230ffc26a09")
     DB_URL = os.environ.get(
-        "DB_URL", )
+        "DB_URL", "mongodb+srv://uvew:xyz@cluster0.qnsajle.mongodb.net/?retryWrites=true&w=majority")
     DB_NAME = os.environ.get("DB_NAME", "FO21664C08986")
     BOT_TOKEN = os.environ.get(
         "BOT_TOKEN", "5820214092:AAGKU17Ww8g34euND-rvL4PnTnos0TyfCsU")
