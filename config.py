45bc9eeafb394ca8e78e5d625203f2ca27f25765
Stradalwn <132915496+Stradalwn@users.noreply.github.com>
2023-05-24 04:19:40 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ class Config:
         'ADMINS', '6069226956 1288398723').split()]
     API_HASH = os.environ.get("API_HASH", "5078bf823d7e8248776f5230ffc26a09")
     DB_URL = os.environ.get(
-        "DB_URL", "mongodb+srv://uvew:xyz@cluster0.qnsajle.mongodb.net/?retryWrites=true&w=majority")
+        "DB_URL", )
     DB_NAME = os.environ.get("DB_NAME", "FO21664C08986")
     BOT_TOKEN = os.environ.get(
         "BOT_TOKEN", "5820214092:AAGKU17Ww8g34euND-rvL4PnTnos0TyfCsU")
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQCdyCKGh1iAMXPHlUWunlzBZPLt5wlOBtqR_lBO3HKqekSuI95NpZGUdkAQm3NLf9lBDQXvQbyaIhZNbMCLj_6eM33Xdk1RDIGMfCiF_QTCZsp4Fcg-9bTZbrcSOVMXeqomvuiTVngqtOF3CSlG4LsKAg2Vyn3tNUCe1225KTHb2-55urAdi5AC45Q8C-7Cy4oVk3uppwP7qWo8OjFHoW1SoK3DGcWIFWMBDPRGukmlmh7fxj71L_yAHUYxVx2qNqIBJNtd5k1urVxShEYf87kamPxSKf3ge6dnmP_r4qKm3-tQjmDdMxjIoALpPnpTgEiSJIUR3I9WW_ps0c0FUGfgAAAAAWnBDcwA")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQCgG9Ct8sWBosee6pom20tI60kbJH9hoQ9hYzZdiJIL0DekKS5H_ZRzQzBAbJJLb0wj_XKE6BS86_FPDTobWYZPwrrcDCvIQBiGemR_Q74YXaXTJ6xXT04-I6PjMJa2fVnt0slE9NdZ0YHRJbPFal9V51LFyPdyl-HJryuG8UJeYnmQj9H9MGpvHZgXYfP-anfFWwgjgSXLEwtv8TyezvEDiDSrnlvsfL1YGMfyq5O7MlVqozrS7f57bsplNYjHTFO06uGHt8pfcsEp-RX8mMe69D4CMV9-Pkdv21cvlh24EaqIIghL-qhEXjg3fFPkTd8T0bcI4u9teP2hUrFcbJQLAAAAAWnBDcwA")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001501263659"))
